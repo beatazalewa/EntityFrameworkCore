339f703f95ca72176a6e998ee29c3f6b83ce2f52
Beata Zalewa <beata.zalewa@gmail.com>
2021-08-21 09:45:56 +0200

    Innitial Data

---
+++ EntityFrameworkCore.Introduction/MovieContext.cs
@@ -0,0 +1,36 @@
+﻿using EntityFrameworkCore.Introduction.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCore.Introduction
+{
+    public class MovieContext : DbContext
+    {
+        private readonly bool _useLazyLoading;
+        public MovieContext(bool useLazyLoading)
+        {
+            _useLazyLoading = useLazyLoading;
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Person> People { get; set; }
+
+        protected override void OnConfiguring (DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            if (_useLazyLoading)
+            {
+                optionsBuilder.UseLazyLoadingProxies().UseInMemoryDatabase("MoviesDb");
+            }
+            else
+            {
+                optionsBuilder.UseInMemoryDatabase("MoviesDb");
+            }
+        }
+    }
+}
