d7ace1b3aeaaee2b52830bbb258792c114190bc4
Beata Zalewa <beata.zalewa@gmail.com>
2021-08-21 11:39:49 +0200

    Added LazyLoading test

--- EntityFrameworkCore.Introduction/RelationsSamples.cs
+++ EntityFrameworkCore.Introduction/RelationsSamples.cs
@@ -109,5 +109,38 @@ namespace EntityFrameworkCore.Introduction
                 person.Movies.Should().HaveCount(3);
             }
         }
+
+        [Fact]
+        public async Task LazyLoading()
+        {
+            using (var context = new MovieContext(true))
+            {
+                var person = new Person { Id = 1, FirstName = "James", LastName = "Cameron" };
+                var genre1 = new Genre { Id = 1, Name = "Dramat" };
+                var genre2 = new Genre { Id = 2, Name = "Akcja" };
+                var movie1 = new Movie { Id = 1, Title = "Titanic", Director = person, Genre = genre1 };
+                var movie2 = new Movie { Id = 2, Title = "Obcy 2", Director = person, Genre = genre2 };
+                var movie3 = new Movie { Id = 3, Title = "Terminator", Director = person, Genre = genre2 };
+                await context.AddAsync(movie1);
+                await context.AddAsync(movie2);
+                await context.AddAsync(movie3);
+                await context.SaveChangesAsync();
+            }
+
+            using (var context = new MovieContext(true))
+            {
+                var movie = await context.Movies.FindAsync(1);
+                movie.Director.LastName.Should().Be("Cameron");
+                movie.Genre.Name.Should().Be("Dramat");
+            }
+
+            using (var context = new MovieContext(true))
+            {
+                var person = await context.People.FindAsync(1);
+
+                person.Movies.Should().NotBeNullOrEmpty();
+                person.Movies.Should().HaveCount(3);
+            }
+        }
     }
 }
