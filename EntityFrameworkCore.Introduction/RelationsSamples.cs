977153b397388b497bf8ce3afe28eddfbafe6f15
Beata Zalewa <beata.zalewa@gmail.com>
2021-08-21 10:58:21 +0200

    Added Relaion test part 2

--- EntityFrameworkCore.Introduction/RelationsSamples.cs
+++ EntityFrameworkCore.Introduction/RelationsSamples.cs
@@ -21,8 +21,20 @@ namespace EntityFrameworkCore.Introduction
                 await context.AddAsync(new Genre { Id = 1, Name = "Dramat"});
                 await context.SaveChangesAsync();
             }
+
+            using (var context = new MovieContext(true))
+            {
+                var person = await context.People.FindAsync(1);
+                var genre1 = await context.Genres.FindAsync(1);
+                var genre2 = new Genre { Id = 2, Name = "Akcja" };
+                var movie1 = new Movie { Id = 1, Title = "Titanic", Director = person, Genre = genre1 };
+                var movie2 = new Movie { Id = 2, Title = "Obcy 2", Director = person, Genre = genre2 };
+                var movie3 = new Movie { Id = 3, Title = "Terminator", Director = person, Genre = genre2 };
+                await context.AddAsync(movie1);
+                await context.AddAsync(movie2);
+                await context.AddAsync(movie3);
+                await context.SaveChangesAsync();
+            }
         }
-
-
     }
 }
