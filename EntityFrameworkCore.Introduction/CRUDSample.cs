ea881d10ccd8b0ae250f32c79e203f5f90bcf662
Beata Zalewa <beata.zalewa@gmail.com>
2021-08-21 10:01:58 +0200

    Added first test

---
+++ EntityFrameworkCore.Introduction/CRUDSample.cs
@@ -0,0 +1,43 @@
+using EntityFrameworkCore.Introduction.Domain;
+using FluentAssertions;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EntityFrameworkCore.Introduction
+{ 
+    //IMPORTANT: Run each "test" separately
+    [Trait("Category", "InMemory")]
+    public class CRUDSample
+    {
+        [Fact]
+        public async Task Add()
+        {
+            using (var context = new MovieContext(true))
+            {
+                var person1 = new Person { Id = 1, FirstName = "John", LastName = "McTiernan" };
+                var person2 = new Person { Id = 2, FirstName = "James", LastName = "Cameron" };
+
+                await context.People.AddAsync(person1);
+                await context.People.AddAsync(person2);
+                await context.SaveChangesAsync();
+            }
+
+            using (var context = new MovieContext(true))
+            {
+                var person1 = await context.People.FindAsync(1);
+                var person2 = await context.People.FindAsync(2);
+
+                person1.Should().NotBeNull();
+                person1.Id.Should().Be(1);
+                person1.FirstName.Should().Be("John");
+                person1.LastName.Should().Be("McTiernan");
+
+                person2.Should().NotBeNull();
+                person2.Id.Should().Be(2);
+                person2.FirstName.Should().Be("James");
+                person2.LastName.Should().Be("Cameron");
+            }
+        }
+    }
+}
