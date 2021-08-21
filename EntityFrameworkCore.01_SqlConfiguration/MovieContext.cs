fad83bc209b5c0ce36d6ec368d16d477bdcbd0ec
Beata Zalewa <beata.zalewa@gmail.com>
2021-08-21 11:59:24 +0200

    Added MovieContext class

---
+++ EntityFrameworkCore.01_SqlConfiguration/MovieContext.cs
@@ -0,0 +1,25 @@
+﻿using EntityFrameworkCore._01_SqlConfiguration.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCore._01_SqlConfiguration
+{
+    public class MovieContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Person> People { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseLazyLoadingProxies()
+                .UseSqlServer("Server=LAPTOP-ZALNET\\SQLEXPRESS;Database=EFCore;TrustedConnection=True;MultipleActiveResultSets=True;");
+            //.UseInMemoryDatabase("EFCore.db");
+        }
+    }
+}
