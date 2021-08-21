d1d773464910a7c008a8cfb95f33419de619277a
Beata Zalewa <beata.zalewa@gmail.com>
2021-08-21 11:50:38 +0200

    Added second project

---
+++ EntityFrameworkCore.01_SqlConfiguration/Domain/Movie.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCore._01_SqlConfiguration.Domain
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public virtual Genre Genre { get; set; } //one
+        public virtual Person Director { get; set; } //one
+    }
+
+    public class Genre
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public virtual IList<Movie> Movies { get; set; } //many
+    }
+}
---
+++ EntityFrameworkCore.01_SqlConfiguration/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace EntityFrameworkCore._01_SqlConfiguration
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
