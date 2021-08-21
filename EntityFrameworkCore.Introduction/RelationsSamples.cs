93a763388c8ea7edc2111057a4d91e6d767114f7
Beata Zalewa <beata.zalewa@gmail.com>
2021-08-21 11:19:39 +0200

    Added EagerLoading test

--- EntityFrameworkCore.Introduction/RelationsSamples.cs
+++ EntityFrameworkCore.Introduction/RelationsSamples.cs
@@ -64,5 +64,22 @@ namespace EntityFrameworkCore.Introduction
                 person.Movies.Should().HaveCount(3);
             }
         }
+
+        public async Task EagerLoading()
+        {
+            using (var context = new MovieContext(true))
+            {
+                var person = new Person { Id = 1, FirstName = "James", LastName = "Cameron" };
+                var genre1 = new Genre { Id = 1, Name = "Dramat" };
+                var genre2 = new Genre { Id = 2, Name = "Akcja" };
+                var movie1 = new Movie { Id = 1, Title = "Titanic", Director = person, Genre = genre1 };
+                var movie2 = new Movie { Id = 2, Title = "Obcy 2", Director = person, Genre = genre2 };
+                var movie3 = new Movie { Id = 3, Title = "Terminator", Director = person, Genre = genre2 };
+                await context.AddAsync(movie1);
+                await context.AddAsync(movie2);
+                await context.AddAsync(movie3);
+                await context.SaveChangesAsync();
+            }
+        }
     }
 }
