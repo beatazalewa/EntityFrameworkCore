522153e3575908632c37a94a441034c02770da46
Beata Zalewa <beata.zalewa@gmail.com>
2021-08-21 13:37:58 +0200

    Added AddYearMigration

--- EntityFrameworkCore.02_Migrations/Migrations/20210821113504_AddYear.Designer.cs
+++ EntityFrameworkCore.02_Migrations/Migrations/20210821113504_AddYear.Designer.cs
@@ -4,14 +4,16 @@ using EntityFrameworkCore._02_Migrations;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EntityFrameworkCore._02_Migrations.Migrations
 {
     [DbContext(typeof(MovieContext))]
-    partial class MovieContextModelSnapshot : ModelSnapshot
+    [Migration("20210821113504_AddYear")]
+    partial class AddYear
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EntityFrameworkCore.02_Migrations/Migrations/20210821113504_AddYear.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EntityFrameworkCore._02_Migrations.Migrations
+{
+    public partial class AddYear : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "YearOfProduction",
+                table: "Movies",
+                type: "int",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "YearOfProduction",
+                table: "Movies");
+        }
+    }
+}
