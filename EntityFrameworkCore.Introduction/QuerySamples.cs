61db7b1aec1822c2a345d456f49eac5d1c01a91b
Beata Zalewa <beata.zalewa@gmail.com>
2021-08-21 10:31:48 +0200

    Added Query test part 1

---
+++ EntityFrameworkCore.Introduction/QuerySamples.cs
@@ -0,0 +1,29 @@
+﻿using EntityFrameworkCore.Introduction.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EntityFrameworkCore.Introduction
+{
+    //IMPORTANT: Run each "test" separately
+    [Trait("Category", "InMemoryQuery")]
+    public class QuerySamples
+    {
+        [Fact]
+        public async Task Query()
+        {
+            using (var context = new MovieContext())
+            {
+                var person = new Person { Id = 1, FirstName = "James", LastName = "Cameron" };
+                var genre1 = new Genre { Id = 1, Name = "Dramat" };
+                var genre2 = new Genre { Id = 2, Name = "Akcja" };
+                var movie1 = new Movie { Id = 1, Title = "Titanic", Director = person, Genre = genre1 };
+                var movie2 = new Movie { Id = 2, Title = "Obcy 2", Director = person, Genre = genre2 };
+                var movie3 = new Movie { Id = 3, Title = "Terminator", Director = person, Genre = genre1 };
+            }
+        }
+    }
+}
